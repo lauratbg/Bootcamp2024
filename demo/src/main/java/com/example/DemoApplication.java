b29a1fa2df5d3e823f2c5456c1229a722e9619f8
lauratbg <lauratbg2001@gmail.com>
2024-06-24 12:34:30 +0200

    dao

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.ioc.Entorno;
-import com.example.ioc.Rango;
-import com.example.ioc.Saluda;
+import com.example.domains.contracts.repositories.ActorRepository;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -17,31 +15,23 @@ public class DemoApplication implements CommandLineRunner {
 	}
 
 	@Autowired
-//	@Qualifier("es")
-	Saluda saluda;
-	@Autowired
-//	@Qualifier("en")
-	Saluda saluda2;
-	@Autowired
-	Entorno entorno;
-	@Autowired
-	private Rango rango;
+	ActorRepository dao;
 
-//	@Autowired(required = false)
-//	SaludaEnImpl kk;
-	
 	@Override
 	public void run(String... args) throws Exception {
 		System.err.println("Aplicación arrancada...");
-//		var saluda = new Saluda();
-		System.out.println(saluda.getContador());
-		saluda.saluda("Mundo");
-//		saluda.saluda(null);
-		saluda2.saluda("Mundo");
-		System.out.println(saluda.getContador());
-		System.out.println(saluda2.getContador());
-		System.out.println(entorno.getContador());
-//		System.out.println(rango.getMin() + " -> " + rango.getMax());
+
+		var item = dao.findById(204);
+
+		if (item.isEmpty())
+			System.err.println("No encontrado");
+		else {
+			var actor = item.get();
+			actor.setFirstName(actor.getFirstName().toUpperCase());
+			dao.save(actor);
+		}
+
+		dao.findAll().forEach(System.out::println);
 	}
 
 }
