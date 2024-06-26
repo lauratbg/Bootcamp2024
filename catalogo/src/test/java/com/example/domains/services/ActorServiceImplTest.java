2d884b73d75b5a8740b78f4f63b5d015048e1fd4
lauratbg <lauratbg2001@gmail.com>
2024-06-26 11:54:19 +0200

    ActorServiceImplTest not working

--- catalogo/src/test/java/com/example/domains/services/ActorServiceImplTest.java
+++ catalogo/src/test/java/com/example/domains/services/ActorServiceImplTest.java
@@ -16,7 +16,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
 
 import com.example.domains.contracts.repositories.ActorRepository;
 import com.example.domains.contracts.services.ActorService;
@@ -25,7 +24,7 @@ import com.example.exceptions.DuplicateKeyException;
 import com.example.exceptions.InvalidDataException;
 
 @DataJpaTest
-@ComponentScan(basePackages = "com.example")
+//@ComponentScan(basePackages = "com.example")
 class ActorServiceImplTest {
 
 	@MockBean
