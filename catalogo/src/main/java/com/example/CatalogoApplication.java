16107ea049ed28924bf22f23d35711809c69c507
lauratbg <lauratbg2001@gmail.com>
2024-07-04 14:43:41 +0200

    done in "catalogo"

--- catalogo/src/main/java/com/example/CatalogoApplication.java
+++ catalogo/src/main/java/com/example/CatalogoApplication.java
@@ -6,7 +6,12 @@ import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.RestTemplate;
 
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -14,6 +19,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import jakarta.transaction.Transactional;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @OpenAPIDefinition(
 		 info = @Info(title = "Microservicio: Catálogo", version = "1.0",
 		 description = "Aprendiendo microservicios."
@@ -28,6 +34,17 @@ public class CatalogoApplication implements CommandLineRunner {
 	 var schemas = openApi.getComponents().getSchemas();
 	 openApi.getComponents().setSchemas(new TreeMap<>(schemas));
 	 };
+	}
+	@Bean 
+	@Primary
+	public RestTemplate restTemplate(RestTemplateBuilder builder) { // sin balancear
+		return builder.build();
+	}
+
+	@Bean 
+	@LoadBalanced
+	public RestTemplate restTemplateLB(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoApplication.class, args);
