4e023ebc04ac2ddcf56e237f5663d84699cdb56b
lauratbg <lauratbg2001@gmail.com>
2024-07-04 14:38:43 +0200

    configured and working

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -6,7 +6,12 @@ import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.RestTemplate;
 
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -15,6 +20,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @OpenAPIDefinition(
 		 info = @Info(title = "Microservicio: Demos", version = "1.0",
 		 description = "**Demos** de Microservicios.",
@@ -36,6 +42,18 @@ public class DemoApplication implements CommandLineRunner {
 			var schemas = openApi.getComponents().getSchemas();
 			openApi.getComponents().setSchemas(new TreeMap<>(schemas));
 		};
+	}
+	
+	@Bean 
+	@Primary
+	public RestTemplate restTemplate(RestTemplateBuilder builder) { // sin balancear
+		return builder.build();
+	}
+	
+	@Bean 
+	@LoadBalanced
+	public RestTemplate restTemplateLB(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 	@Override
