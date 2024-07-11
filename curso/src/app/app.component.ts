2f0c77bd2d1e3d56f58b8e7ae3282e406ba67d24
lauratbg <lauratbg2001@gmail.com>
2024-07-11 08:48:20 +0200

    fixed little bug

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SecurityModule } from './security';
-import { LoggerService, MyCoreModule } from '@my/core';
+import {  MyCoreModule } from '@my/core';
 // import { NotificationComponent } from './main/notification/notification.component';
 import { CommonModule } from '@angular/common';
 import { DemosComponent } from './demos/demos.component';
@@ -15,11 +15,11 @@ import { NotificationModalComponent } from './main';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title: string = 'world';
-  constructor(log: LoggerService) {
-    log.error('Es un error');
-    log.warn('Es un warn');
-    log.info('Es un info');
-    log.log('Es un log');
-  }
+  // title: string = 'world';
+  // constructor(log: LoggerService) {
+  //   log.error('Es un error');
+  //   log.warn('Es un warn');
+  //   log.info('Es un info');
+  //   log.log('Es un log');
+  // }
 }
