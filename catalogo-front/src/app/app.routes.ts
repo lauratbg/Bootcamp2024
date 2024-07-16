cb378818bca975eee3c947b840b6db7cd6d1a2e4
lauratbg <lauratbg2001@gmail.com>
2024-07-16 13:38:28 +0200

    'peliculas' componen basis working

--- catalogo-front/src/app/app.routes.ts
+++ catalogo-front/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent, PageNotFoundComponent } from './main';
+import { PeliculasComponent } from './peliculas/peliculas.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'inicio', component: HomeComponent },
+  { path: 'peliculas', component: PeliculasComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
---
+++ catalogo-front/src/app/peliculas/peliculas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-peliculas',
+  standalone: true,
+  imports: [],
+  templateUrl: './peliculas.component.html',
+  styleUrl: './peliculas.component.css'
+})
+export class PeliculasComponent {
+
+}
